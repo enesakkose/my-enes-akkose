97c286841068f0e8dfc04f169cd1f43f063860f7
enesakkose <ens.akkose@gmail.com>
2026-01-12 00:37:28 +0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="text-3xl font-bold">enes akköse</h1>
+      <h1 className="text-3xl font-bold">enes akköse ve enes akköse</h1>
     </div>
   );
 }
